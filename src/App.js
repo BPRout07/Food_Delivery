3f52006ace3448ec77133fd68345fbb39efde988
Barada Prasad Rout <baradaprasadrout1@gmai.com>
2023-10-08 12:25:02 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './about.css';
 import './loader.css';
 
-$(window).on('load',function() {'use strict';
-$(".loader").fadeOut("slow");
-});
+// $(window).on('load',function() {'use strict';
+// $(".loader").fadeOut("slow");
+// });
 function App() {
   return (
     <div className="App">
